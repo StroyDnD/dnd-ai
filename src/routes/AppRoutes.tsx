caa54afe540436d9b8c77e4e1d6f5428f8f91077
Larry Gust <larry.v.gust@gmail.com>
2025-04-25 16:38:36 -0600

    auth provider w supabase setup

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,18 +1,23 @@
+import { AuthModal } from "@/components/AuthModal";
 import { Layout } from "@/components/Layout";
+import { Navbar } from "@/components/Navbar";
 import Campaign from "@/pages/Campaign";
 import CreateStory from "@/pages/CreateStory";
 import { Home } from "@/pages/Home";
 import { Route, Routes } from "react-router";
-
 export const AppRoutes = () => {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-campaign" element={<CreateStory />} />
-        <Route path="/campaign" element={<Campaign />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
-      </Route>
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-campaign" element={<CreateStory />} />
+          <Route path="/campaign" element={<Campaign />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Route>
+      </Routes>
+      <AuthModal />
+    </>
   );
 };
